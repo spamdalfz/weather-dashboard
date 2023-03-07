7ca2dadb9d9279c969ec74d4fb36777055e1851b
spamdalfz <coreyvasser@gmail.com>
2023-03-07 01:04:02 -0800

    added function to display 5 day forecast.

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,43 @@ searchButton.addEventListener("click", function (event) {
                     return response.json();
                 })
                 .then(function (response) {
-                    console.log(response);
+                    // Update HTML with forecast data
+                    var forecastRow = document.querySelector(".forecast-row");
+                    forecastRow.innerHTML = "";
+
+                    // Loop through forecast data and create HTML elements for each forecast entry
+                    for (var i = 0; i < response.list.length; i++) {
+                        var forecastEntry = response.list[i];
+
+                        // Get date and time
+                        var timestamp = forecastEntry.dt;
+                        var date = new Date(timestamp * 1000);
+                        var dateString = date.toLocaleDateString();
+
+                        // Get weather icon and description
+                        var iconCode = forecastEntry.weather[0].icon;
+                        var description = forecastEntry.weather[0].description;
+
+                        // Get temperature (in Fahrenheit)
+                        var temperature = (forecastEntry.main.temp - 273.15) * 9 / 5 + 32;
+                        temperature = temperature.toFixed(1);
+
+
+                        // Create HTML element for forecast entry
+                        var forecastEntryHtml = `
+                            <div class="col-md-2 forecast-entry">
+                                <div class="forecast-date">${dateString}</div>
+                                <div class="forecast-icon">
+                                    <img src="http://openweathermap.org/img/w/${iconCode}.png" alt="${description}">
+                                </div>
+                                <div class="forecast-description">${description}</div>
+                                <div class="forecast-temperature">${temperature} &deg;F</div>
+                            </div>
+                        `;
+
+                        // Append HTML element to forecast section
+                        forecastRow.innerHTML += forecastEntryHtml;
+                    }
                 })
         });
 });
