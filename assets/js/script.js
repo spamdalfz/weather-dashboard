c3a497dc8c9b4e5e50a30ec490ba44c1d9fa1ae8
spamdalfz <coreyvasser@gmail.com>
2023-03-07 00:02:27 -0800

    fixed some .js issues/

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,11 @@ $(document).ready(function () {
     $("#search-button").on("click", function (event) {
         event.preventDefault();
 
-        // Save search query to local storage
         var cityName = $("#cityName").val();
-        saveSearchQuery(cityName);
 
         // Clear search bar
         $("#cityName").val("");
+
         // Convert city name to latitude and longitude using OpenWeatherMap API
         var apiKey = "e7d5a956a7a8f88243a72f1dd3fe8678";
         var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
@@ -31,38 +30,9 @@ $(document).ready(function () {
                 // Display 5 day weather forecast
                 console.log(response);
             });
+
+            // Update recent searches display
         });
-        // Update recent searches display
-        displayRecentSearches();
-
-
-
     });
 
-    // Function to save search query to local storage
-    function saveSearchQuery(query) {
-        var searches = localStorage.getItem("recentSearches");
-        if (searches) {
-            searches = JSON.parse(searches);
-        } else {
-            searches = [];
-        }
-        searches.unshift(query);
-        localStorage.setItem("recentSearches", JSON.stringify(searches));
-    }
-
-    // Function to display recent searches in the "recent-searches" container
-    function displayRecentSearches() {
-        var searches = JSON.parse(localStorage.getItem("recentSearches"));
-        $("#recent-searches").empty();
-        if (searches) {
-            for (var i = 0; i < searches.length; i++) {
-                var li = $("<li></li>").text(searches[i]);
-                $("#recent-searches").append(li);
-            }
-        }
-    }
-
-    // Display initial recent searches on page load
-    displayRecentSearches();
 });
