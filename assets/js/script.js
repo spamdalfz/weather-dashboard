2d116a6c31ee35feb7779616a7df1f2b17fb2caa
spamdalfz <coreyvasser@gmail.com>
2023-03-07 00:17:24 -0800

    removed the jquery from the js functions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,37 @@
-$(document).ready(function () {
 
-    // Event listener for search button
-    $("#search-button").on("click", function (event) {
-        event.preventDefault();
+var searchButton = document.querySelector("#search-button");
 
-        var cityName = $("#cityName").val();
+// Add event listener for search button
+searchButton.addEventListener("click", function (event) {
+    event.preventDefault();
 
-        // Clear search bar
-        $("#cityName").val("");
+    // Get city name input
+    var cityName = document.querySelector("#cityName").value;
 
-        // Convert city name to latitude and longitude using OpenWeatherMap API
-        var apiKey = "e7d5a956a7a8f88243a72f1dd3fe8678";
-        var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+    // Clear search bar
+    document.querySelector("#cityName").value = "";
 
-        $.ajax({
-            url: queryUrl,
-            method: "GET"
-        }).then(function (response) {
+    // Convert city name to latitude and longitude using OpenWeatherMap API
+    var apiKey = "e7d5a956a7a8f88243a72f1dd3fe8678";
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+
+    fetch(queryUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
             var lat = response.coord.lat;
             var lon = response.coord.lon;
 
             // Use latitude and longitude to get 5 day weather forecast using OpenWeatherMap API
             var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
 
-            $.ajax({
-                url: forecastUrl,
-                method: "GET"
-            }).then(function (response) {
-                // Display 5 day weather forecast
-                console.log(response);
-            });
-
-            // Update recent searches display
+            fetch(forecastUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (response) {
+                    console.log(response);
+                })
         });
-    });
-
 });
