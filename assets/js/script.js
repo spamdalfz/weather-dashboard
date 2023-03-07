e5c6920cb7051142fba6670e642563982c07bea8
spamdalfz <coreyvasser@gmail.com>
2023-03-06 17:32:09 -0800

    added api calls to openweather to get forcast info and convert city to lat and long.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,33 @@ $(document).ready(function () {
 
         // Clear search bar
         $("#cityName").val("");
+        // Convert city name to latitude and longitude using OpenWeatherMap API
+        var apiKey = "e7d5a956a7a8f88243a72f1dd3fe8678";
+        var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
 
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+
+            // Use latitude and longitude to get 5 day weather forecast using OpenWeatherMap API
+            var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+
+            $.ajax({
+                url: forecastUrl,
+                method: "GET"
+            }).then(function (response) {
+                // Display 5 day weather forecast
+                console.log(response);
+            });
+        });
         // Update recent searches display
         displayRecentSearches();
+
+
+
     });
 
     // Function to save search query to local storage
